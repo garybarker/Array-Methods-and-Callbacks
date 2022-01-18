7cc41b5615d1b8355c4693b7cc2e1130b900bcd1
Gary Barker <garybarker.19894@gmail.com>
2022-01-18 16:28:24 -0500

    4 tests passed

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const { fifaData } = require('./fifa.js')
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
-
+const finals2014 = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final'
+});
+console.log('task1', finals2014);
 //(a) Home Team name for 2014 world cup final
-
+console.log('task1a', finals2014[0]['Home Team Name']);
 //(b) Away Team name for 2014 world cup final
-
+console.log('task1b', finals2014[0]['Away Team Name']);
 //(c) Home Team goals for 2014 world cup final
-
+console.log('task1c', finals2014[0]['Home Team Goals']);
 //(d) Away Team goals for 2014 world cup final
-
+console.log('task1d', finals2014[0]['Away Team Goals']);
 //(e) Winner of 2014 world cup final */
 
 
@@ -24,8 +27,11 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(array) {
+   const newArray = array.filter(function(item){
+       return item.Stage === 'Final';
+   });
+   return newArray;
 }
 
 
@@ -36,8 +42,9 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, callback) {
+    const years = callback(array).map(item => item.Year);
+    return years;
 }
 
 
@@ -49,9 +56,17 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-    /* code here */
-}
+function getWinners(array, callback) {
+  const winners = [];
+  callback(array).map(item => {
+      if(item['Home Team Goals'] > item['Away Team Goals']){
+          return winners.push(item['Home Team Name']);
+      }else{
+          return winners.push(item['Away Team Name']);
+      }
+    })
+  return winners;
+  }
 
 
 
